ab7b7f28bc51e241e85c26a5bdf018f6ac79df39
Scott Bergler <commandinghands@gmail.com>
2021-08-22 09:53:35 -0700

    WIP: Trying to get character homeworlds

--- src/App.js
+++ src/App.js
@@ -7,14 +7,26 @@ import SwapiTable from "./SwapiTable";
 function App() {
   const swapi = new Api();
   const [characters, setCharacters] = React.useState([]);
-  const [species, setSpecies] = React.useState([]);
-  React.useEffect(async () => {
-    setCharacters(await swapi.getPeople());
+  const [planets, setPlanets] = React.useState({});
+
+  React.useEffect(() => {
+    swapi
+      .getPeople()
+      .then((resp) => resp.data.results)
+      .then((people) => {
+        people.map((person) => {
+          swapi.getPlanet(person.homeworld).then((planet) => {
+            person.homeworld = planet.data.name;
+          });
+        });
+        setCharacters(people);
+      });
   }, []);
 
-  if (characters.length === 0) return null;
+  if (!characters || characters.length === 0) return null;
   console.log("Characters", characters);
-  console.log("Species", species);
+  console.log("Planets", planets);
+
   return (
     <main>
       <h1>STAR WARS CHARACTER SEARCH</h1>
--- src/apiService.js
+++ src/apiService.js
@@ -1,30 +1,26 @@
 import axios from "axios";
 
 class Swapi {
+  people = [];
   constructor() {
     this.instance = axios.create({
       baseURL: "https://swapi.dev/api/",
     });
   }
 
-  async getPeople() {
-    let people = [];
-    await this.instance.get("people").then((resp) => {
-      people = resp.data.results;
-    });
-    return people;
+  // returns Promise<AxiosResponse<any>>
+  getPeople() {
+    return this.instance.get("people");
   }
 
+  // returns Promise<AxiosResponse<any>>
   getPlanets() {
     return this.instance.get("planets");
   }
 
-  getPlanet(id) {
-    return this.instance.get(`planets/${id}`);
-  }
-
-  getSpecies() {
-    return this.instance.get("species");
+  // returns Promise<AxiosResponse<any>>
+  getPlanet(url) {
+    return this.instance.get(url);
   }
 }
 
